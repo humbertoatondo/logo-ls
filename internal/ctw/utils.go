d6e8f152fabd9f45bda6d3689a1b3a62006590be
Humberto Atondo <humbertoatondo@Humbertos-MacBook-Pro.local>
2021-12-26 11:56:05 -0600

    Replace braile dots unicdoe with space

--- internal/ctw/utils.go
+++ internal/ctw/utils.go
@@ -6,7 +6,7 @@ var (
 	noColor    string = "\033[0m"
 	green      string = "\033[38;2;055;183;021m"
 	brown      string = "\033[38;2;192;154;107m"
-	brailEmpty string = "\u2800"
+	brailEmpty string = " "
 )
 
 func DisplayColor(b bool) {
